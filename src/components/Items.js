1127dada17a5ca2e27ca90a1f70db60f903ba661
erikleisinger <erikleisinger@gmail.com>
2020-11-21 09:40:19 -0500

    character items display, now multiple items aligned like an inventory

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,23 +1,28 @@
-import Axios from "axios"
-import axios from 'axios'
-import { useEffect, useState } from "react"
+import './Items.scss'
+import Item from './Item'
+import className from 'classnames'
 
 
 export default function Items (props) {
-
-  const [items, setItems ] = useState('')
-
-  useEffect(() => {
-    axios.get('http://localhost:3030/items')
-    .then((data) => setItems(data.data[0]))
-    .catch((err) => console.log(err))
-  }, [])
-
-  if (items) {
-    items.items.items.map((item) => {
-      console.log(item.name)
+  
+  const items = props.items.map((item) => {
+    const itemType = className({
+      'inventory-helm': item.inventoryId === 'Helm',
+      "inventory-armour": item.inventoryId === 'BodyArmour',
+      "inventory-gloves": item.inventoryId === 'Gloves',
+      "inventory-boots": item.inventoryId === 'Boots',
+      "inventory-ring": item.inventoryId === 'Ring',
+      "inventory-ring2": item.inventoryId === 'Ring2',
+      "inventory-offhand2": item.inventoryId === 'Offhand2',
+      "inventory-amulet": item.inventoryId === 'Amulet',
+      "inventory-belt":item.inventoryId === 'Belt',
+      "inventory-weapon":item.inventoryId === 'Weapon'
     })
-  }
+      return <div className={itemType}><Item item={item} /></div>
     
-  return <div />
+  })
+ 
+  return <div className="inventory">
+      {items}
+  </div>
 }
