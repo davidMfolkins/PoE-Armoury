7e973c3d8101d989c91fca49bf8ee2e3136f0cf4
David Folkins <david.folkins88@gmail.com>
2020-11-20 21:20:19 -0500

    small format

--- src/components/Ladder.js
+++ src/components/Ladder.js
@@ -7,49 +7,47 @@ import { Table } from 'react-bootstrap'
 
 
 function Ladder() {
-  const [data, setData] = useState ({
+  const [data, setData] = useState({
     ladderChars: []
   })
 
   useEffect(() => {
     axios.get('http://localhost:3030/ladder')
-    .then( (result) => {
-    
-      setData({...data, ladderChars: result.data[0].rankings.entries})
-      console.log(result)
-    })
-  }, []) 
+      .then((result) => {
+
+        setData({ ...data, ladderChars: result.data[0].rankings.entries })
+        console.log(result)
+      })
+  }, [])
 
   const rows = data.ladderChars.map((entry) => {
     const className = entry.character.class
     return (
       <tr className="d-flex">
-      <td className="col-4">{entry.character.name}</td>
-      <td className="col-4">{entry.character.level}</td>
-      <td className="col-4">{className}</td>
-    </tr>
+        <td className="col-4">{entry.character.name}</td>
+        <td className="col-4">{entry.character.level}</td>
+        <td className="col-4">{className}</td>
+      </tr>
     )
   })
 
 
- 
+
   return (
     <div className="ladderContainer">
-      
       <Table striped bordered hover variant="dark">
-  <thead>
-    <tr className="d-flex">
+        <thead>
+          <tr className="d-flex">
+            <th className="col-4">Name</th>
+            <th className="col-4">Level</th>
+            <th className="col-4">Class</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
 
-      <th className="col-4">Name</th>
-      <th className="col-4">Level</th>
-      <th className="col-4">Class</th>
-    </tr>
-  </thead>
-  <tbody>
-    {rows}
-
-  </tbody>
-</Table>
+        </tbody>
+      </Table>
     </div>
   );
 }
