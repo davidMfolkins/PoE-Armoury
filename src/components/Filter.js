4a60504deb0ef988cb1123c74ca1db6832eaf784
David Folkins <david.folkins88@gmail.com>
2020-11-22 15:26:55 -0500

    add accordian to filter button

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Form, FormControl, Accordion, Card } from 'react-bootstrap'
 
 import './Filter.scss'
 
 function Filter(props) {
+
+  const [value, setValue] = useState("")
   return (
-    <div className="filterContainer">
-      <div>Filter</div>
-    </div>
+    <Accordion className="accordianContainer">
+      <Card>
+        <Accordion.Toggle variant="link" eventKey="0">
+          Filters
+      </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            <div className="filterContainer">
+              <Form className="my-2" autocomplete="off">
+                <div>Class: </div>
+                <FormControl type="text" placeholder="Class" name="search" value={value} />
+                <div>Range: </div>
+                <FormControl type="text" placeholder="Min Level" name="search" value={value} />
+                <FormControl type="text" placeholder="Max Level" name="search" value={value} />
+              </Form>
+            </div>
+          </Card.Body>
+        </Accordion.Collapse>
+
+      </Card>
+    </Accordion>
   )
 }
 
