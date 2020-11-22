7174cd4c79c723167ac9eb762d5d3920978bf334
Panini <alexanderscaini@gmail.com>
2020-11-22 14:59:12 -0500

    added an example character that has a non gem in a socket

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,8 +4,23 @@ import className from 'classnames'
 
 
 export default function Skills (props) {
-  
+  const items = props.items.map((item) => {
+    if (item.socketedItems) {
+     const gems = item.socketedItems.map((gem) =>{
+       if(gem.abyssJewel) {
+
+       } else {
+         return <Skill gem={gem}/>
+       }
+     }) 
+    }
+  })
+ 
   return (
-    <></>
+  <div className="inventory-container">
+    <span>
+      {gems}
+    </span>
+  </div>
   )
 }
