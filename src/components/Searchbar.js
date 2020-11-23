ad31aad517ed57371edd6759acdcc0c49992ef83
erikleisinger <erikleisinger@gmail.com>
2020-11-23 09:26:30 -0500

    cleaned up functions/code no longer in use

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,65 +5,11 @@ import { Form, FormControl, Table } from "react-bootstrap";
 
 import "./Searchbar.scss";
 
-const account = [
-  {
-    name: "SoundsOfViolence",
-    league: "Heist",
-    classId: 4,
-    ascendancyClass: 3,
-    class: "Champion",
-    level: 97,
-    experience: 3447548753,
-    lastActive: true,
-  },
-  {
-    name: "pestoOnRice",
-    league: "Heist",
-    classId: 2,
-    ascendancyClass: 3,
-    class: "Pathfinder",
-    level: 85,
-    experience: 1329447985,
-  },
-  {
-    name: "DeadlineWithMyMaker",
-    league: "Heist",
-    classId: 1,
-    ascendancyClass: 3,
-    class: "Chieftain",
-    level: 42,
-    experience: 23151450,
-  },
-  {
-    name: "TheSnowGoose",
-    league: "Heist",
-    classId: 3,
-    ascendancyClass: 1,
-    class: "Occultist",
-    level: 89,
-    experience: 1784977296,
-  },
-];
-
 let charName = "";
 function Searchbar(props) {
   const [value, setValue] = useState("");
 
   const [searchResults, setSearchResults] = useState([]);
-
-  useEffect(() => {}, [value]);
-
-  const search = function (name) {
-    account.map((entry) => {
-      if (name === entry.name) {
-        console.log(name);
-        charName = name;
-        return charName;
-      } else {
-        console.log("No name found");
-      }
-    });
-  };
 
   const quickSearch = async function (e) {
     setValue(e.target.value);
@@ -79,8 +25,7 @@ function Searchbar(props) {
           );
       })
     })
-    
-    console.log(e.target.value);
+  
     if (!e.target.value) {
       setSearchResults([null]);
     } else {
