b4d10b00e77f9727a17a12bb3f920e56cc97ceef
David Folkins <david.folkins88@gmail.com>
2020-11-27 13:41:37 -0500

    small fix for ladder

--- src/components/Ladder.js
+++ src/components/Ladder.js
@@ -37,8 +37,8 @@ let rows;
       return (
         <tr id="ladderList" className="d-flex">
           <td className="col-2"><img src={classIcon} alt={entry.character.name} /></td>
-          <td className="col-4" onClick={() => handleCharacterChange(entry.account.name, entry.character.name, entry.character.id)}>{entry.character.name} </td>
-          <td className="col-1">{entry.character.level}</td>
+          <td className="col-3" onClick={() => handleCharacterChange(entry.account.name, entry.character.name, entry.character.id)}>{entry.character.name} </td>
+          <td className="col-2">{entry.character.level}</td>
           <td className="col-2">{className}</td>
       <td className="col-2">{entry.account.twitch && <a href={`https://twitch.tv/${entry.account.twitch.name}`} target="_blank" rel="noreferrer">{entry.account.twitch.name}</a>}</td>
       <td className="col-1">{props.cookies.user  && <LikeButton characterid={entry.character.id} favourites={props.favourites} character={entry.character} removeFavourite={props.removeFavourite} addFavourite={props.addFavourite} size="1.5em" setMsg={props.setMsg}/>}</td>
@@ -99,8 +99,8 @@ let rows;
           <thead>
             <tr className="d-flex">
               <th className="col-2">Icon</th>
-              <th className="col-4">Name</th>
-              <th className="col-1">Level</th>
+              <th className="col-3">Name</th>
+              <th className="col-2">Level</th>
               <th className="col-2">Class</th>
               <th className="col-2">Twitch</th>
               <th className="col-1">Fav</th>
