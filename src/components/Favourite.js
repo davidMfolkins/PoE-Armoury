e7d747306e6543189f8ebaeabafcab49b6a79d41
erikleisinger <erikleisinger@gmail.com>
2020-11-28 18:58:55 -0500

    small fixes on the favourites page

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -33,7 +33,7 @@ export default function Favourite(props) {
   if (!msg) {
   return (
     <tr id="favouriteList" className="d-flex">
-      <td id="classCell" className="col-3"><img src={classIcon} alt={props.fav.class} /> {props.fav.class}</td>
+      <td id="classCell" className="col-3"><div><img src={classIcon} alt={props.fav.class} /> {props.fav.class}</div></td>
       <td className="col-3" onClick={(e) => {
         e.preventDefault()
         handleCharacterChange(props.fav.account_name, props.fav.name, props.fav.id)
@@ -41,9 +41,9 @@ export default function Favourite(props) {
       }>{props.fav.name}</td>
       <td className="col-3">{props.fav.level}</td>
       <td className="col-3">
-        <Button onClick={() => removeCheck()} variant="primary" size="lg">
-          <AiFillDelete />
-        </Button>{' '}
+        
+          <AiFillDelete size="2em" onClick={() => removeCheck()}  />
+
       </td>
     </tr>
 
