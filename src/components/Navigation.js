4ced81d2ec9f238f239a378c0380fec150b871de
David Folkins <david.folkins88@gmail.com>
2020-11-29 14:31:40 -0500

    remove my account from drop down

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -19,8 +19,6 @@ export default function Navigation(props) {
         {props.cookies.user &&
           <NavDropdown className="d-none d-lg-block" title="Account" id="basic-nav-dropdown">
             <NavDropdown.Item onClick={() => props.setState('favourites')}>Favourites</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">My Account</NavDropdown.Item>
-
             <NavDropdown.Divider />
             <NavDropdown.Item href="/logout" onClick={(e) => props.removeCookie("user")}>Logout</NavDropdown.Item>
           </NavDropdown>
@@ -28,7 +26,6 @@ export default function Navigation(props) {
         {props.cookies.user &&
           <Nav className="d-lg-none">
             <Nav.Link className="mx-3 my-2" onClick={() => props.setState('favourites')}>Favourites</Nav.Link>
-            <Nav.Link className="mx-3 my-2">My Account</Nav.Link>
             <Nav.Link className="mx-3 my-2" href="/logout" onClick={(e) => {
               props.removeCookie("user")
             }}>Logout</Nav.Link>
