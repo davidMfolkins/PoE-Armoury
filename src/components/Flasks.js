4c0e6a1a985632e9b4b0778fb067d30f8504a227
erikleisinger <erikleisinger@gmail.com>
2020-11-30 13:52:54 -0500

    flask, flasks, grabber

--- src/components/Flasks.js
+++ src/components/Flasks.js
@@ -1,14 +1,12 @@
-import Item from './Item'
+import Item from "./Item";
 
-import './Flasks.scss'
+import "./Flasks.scss";
 
 export default function Flasks(props) {
   const flasks = props.items.map((item) => {
-    if (item.inventoryId === 'Flask') {
-      return <Item windowWidth={props.windowWidth} item={item} />
+    if (item.inventoryId === "Flask") {
+      return <Item windowWidth={props.windowWidth} item={item} />;
     }
-  })
-  return (
-    <div className="flasks-container">{flasks}</div>
-  )
+  });
+  return <div className="flasks-container">{flasks}</div>;
 }
