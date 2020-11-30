6b555a38ec6bfed029bdfb734d0c2ca53c7bae86
erikleisinger <erikleisinger@gmail.com>
2020-11-30 14:57:33 -0500

    skills refactor

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,108 +1,126 @@
-import Skill from './Skill'
-import className from 'classnames'
-import { Table } from 'react-bootstrap'
+import Skill from "./Skill";
+import { Col } from "react-bootstrap";
 
-import { Row, Col } from 'react-bootstrap'
+export default function Skills(props) {
 
-export default function Skills (props) {
-
-  // console.log(props.item.inventoryId)
+  // looks in items for sockets and moves them to an array
   let groups = [];
   for (const socket of props.item.sockets) {
-    if (socket.attr !== "A")
-    groups.push(socket.group)
+    if (socket.attr !== "A") groups.push(socket.group);
   }
-  // console.log("groups:" , groups)
-  
+
+  // linkedGroups outputs an array with the number of sockets linked together in the item
   let linkedGroups = [];
   let current = 0;
   let count = 0;
   for (const socket of groups) {
-    if (socket === current){
-      count++
+    if (socket === current) {
+      count++;
     } else {
-      linkedGroups.push(count)
-      current = socket
-      count = 1
+      linkedGroups.push(count);
+      current = socket;
+      count = 1;
     }
   }
-  linkedGroups.push(count)
+  linkedGroups.push(count);
 
-  // console.log("linkedGroups:" , linkedGroups)
-
-  let finalArray = []
+  // finalArray is the number of sockets linked together followed by 0s for the number of links ie [4,0,0,0,2,0]
+  let finalArray = [];
   for (const link of linkedGroups) {
-    let numberOfTimes = link
-    finalArray.push(link)
+    let numberOfTimes = link;
+    finalArray.push(link);
     while (numberOfTimes > 1) {
-      finalArray.push(0)
-      numberOfTimes--
+      finalArray.push(0);
+      numberOfTimes--;
     }
   }
-  // console.log("finalArray:", finalArray)
 
   const gems = props.gems.map((gem, gemIndex) => {
     if (!gem.abyssJewel) {
       return gem;
     }
-  })
+  });
 
-
+    // constructs the grid with links on the left and rows of gems on the right to hand to the render 
   const builder = finalArray.map((connections, index) => {
-
-    const gemColour = props.item.sockets[index].sColour
-    const gemSocket = gems.find(g => g.socket === index)
+    const gemColour = props.item.sockets[index].sColour;
+    const gemSocket = gems.find((g) => g.socket === index);
 
     let level;
     let quality = null;
     if (gemSocket) {
-      gemSocket.properties.map(property => {
+      gemSocket.properties.map((property) => {
         if (property.name === "Level") {
-          return level = property.values[0][0].slice(0,2)
+          return (level = property.values[0][0].slice(0, 2));
         }
-      })
-      gemSocket.properties.map(property => {
+      });
+      gemSocket.properties.map((property) => {
         if (property.name === "Quality") {
-          return quality = property.values[0][0].slice(1,3)
+          return (quality = property.values[0][0].slice(1, 3));
         }
-      })
+      });
     }
 
-
-    if (gems.some(g => g.socket === index)) {
-      // console.log(connections, gemSocket.socket)
+    if (gems.some((g) => g.socket === index)) {
       if (connections) {
+        // if the grid needs to output a new link section and a gem
         return (
           <>
             <div className={"links span" + connections}></div>
-            <div className="gemRow"><Skill gem={gemSocket}/> <div className="gemName">{gemSocket.typeLine} <div className="gemStats">(Level: {level} / Quality: {quality || "0"})</div></div></div>
+            <div className="gemRow">
+              <Skill gem={gemSocket} />{" "}
+              <div className="gemName">
+                {gemSocket.typeLine}{" "}
+                <div className="gemStats">
+                  (Level: {level} / Quality: {quality || "0"})
+                </div>
+              </div>
+            </div>
           </>
-        )
+        );
+        // if there is a gem to output but not a new link section
       } else {
         return (
-          <div className="gemRow"><Skill gem={gemSocket}/> <div className="gemName">{gemSocket.typeLine} <div className="gemStats">(Level: {level} / Quality: {quality || "0"})</div></div></div>
-        )
+          <div className="gemRow">
+            <Skill gem={gemSocket} />{" "}
+            <div className="gemName">
+              {gemSocket.typeLine}{" "}
+              <div className="gemStats">
+                (Level: {level} / Quality: {quality || "0"})
+              </div>
+            </div>
+          </div>
+        );
       }
     } else {
-      // console.log(connections, "x")
+      // if there is a links section but an empty gem
       if (connections) {
         return (
           <>
             <div className={"links span" + connections}></div>
-            <div className="gemRow empty"><div className={"socketColour" + gemColour}/>Empty Socket</div>
+            <div className="gemRow empty">
+              <div className={"socketColour" + gemColour} />
+              Empty Socket
+            </div>
           </>
-        )
+        );
+        // if there is no new links section and an empty gem
       } else {
-        return <div className="gemRow empty"><div className={"socketColour" + gemColour}/>Empty Socket</div>
+        return (
+          <div className="gemRow empty">
+            <div className={"socketColour" + gemColour} />
+            Empty Socket
+          </div>
+        );
       }
     }
-  })
+  });
 
   return (
-  <Col lg={3} xs={6} className="skills-container">
-      <hr className="line"/> 
+    <Col lg={3} xs={6} className="skills-container">
+      <hr className="line" />
       <div className="gemGroup">{props.item.inventoryId}</div>
       <div className="skill-group">{builder}</div>
-  </Col>
-  )
+    </Col>
+  );
 }
