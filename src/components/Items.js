9a7a85f8d2a84ed3164b164672a3f027372e2932
erikleisinger <erikleisinger@gmail.com>
2020-11-30 14:04:04 -0500

    items refactor

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,21 +1,17 @@
-import './Items.scss'
-import Item from './Item'
-import className from 'classnames'
-
+import Item from "./Item";
+import "./Items.scss";
 
 export default function Items(props) {
   const items = props.items.map((item) => {
-    if (item.inventoryId === 'Flask') {
-
+    if (item.inventoryId === "Flask") {
     } else {
-      return <Item windowWidth={props.windowWidth} item={item} />
+      return <Item windowWidth={props.windowWidth} item={item} />;
     }
+  });
 
-  })
-
-  return <div className="inventory-container">
-    <span>
-      {items}
-    </span>
-  </div>
+  return (
+    <div className="inventory-container">
+      <span>{items}</span>
+    </div>
+  );
 }
