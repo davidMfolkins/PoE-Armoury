423308bca9cf8d1d6b6f61b665dec7d69c10ecb5
erikleisinger <erikleisinger@gmail.com>
2020-11-30 14:27:07 -0500

    logout login refactor

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,12 +1,11 @@
-import './Logout.scss';
 import { Container } from 'react-bootstrap'
+import './Logout.scss';
 
 export default function Logout(props) {
   return (
     <Container>
       <div className="logout-container">
         <div className="container logout-form-container">
-
           You have been logged out successfully
       </div>
       </div>
