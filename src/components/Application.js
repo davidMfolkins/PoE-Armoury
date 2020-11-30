5fb2a16f633eeb7ebb3c97fecd7799f531456f9a
erikleisinger <erikleisinger@gmail.com>
2020-11-30 12:20:35 -0500

    application.js refactor

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,151 +1,185 @@
-import "../index.scss";
 import Ladder from "./Ladder";
 import Loading from "./Loading";
 import Character from "./Character";
 import Account from "./Account";
 import Navigation from "./Navigation";
-import Register from './Register';
-import Logout from './Logout';
-import Login from './Login'
-import Favourites from './Favourites';
-import Grabber from './Grabber'
-import Message from './Message'
-import { fetchCharacter  } from "./helpers/getters";
-import Container from "react-bootstrap/Container";
+import Register from "./Register";
+import Logout from "./Logout";
+import Login from "./Login";
+import Favourites from "./Favourites";
+import Grabber from "./Grabber";
+import Message from "./Message";
+import { fetchCharacter } from "./helpers/getters";
+
 import { useState, useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import { useCookies } from 'react-cookie';
+import { useCookies } from "react-cookie";
+import axios from "axios";
+
+import "../index.scss";
+import { CgChevronDoubleLeft } from "react-icons/cg";
 import ScrollUpButton from "react-scroll-up-button";
-import axios from 'axios'
+import Container from "react-bootstrap/Container";
 
-import { CgChevronDoubleLeft } from 'react-icons/cg'
-
-const ladder_standard = require('../ladder_standard.json');
-const ladder_hardcore = require('../ladder_hardcore.json');
-
-
-
+const ladder_standard = require("../ladder_standard.json");
+const ladder_hardcore = require("../ladder_hardcore.json");
 
 export default function Application() {
-
-    
   const [cookies, setCookie, removeCookie] = useCookies(null);
-
 
   function handleCookie(key) {
     setCookie("user", key, {
-      path: "/"
+      path: "/",
     });
   }
-
+    // the visual state of the app:
+      // - 'ladder', 'account', 'character', 'favourites', 'loading'
   const [state, setState] = useState("ladder");
+    // The name of the account currently being searched
   const [account, setAccount] = useState("");
+    // an object with account and character information of character being inspected
   const [character, setCharacter] = useState(null);
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [favourites, setFavourites] = useState([])
-  const [loadingMsg, setLoadingMsg] = useState(null)
-  const [loadingError, setLoadingError] = useState(null)
-  const [standardLadder, setStandardLadder] = useState([])
-  const [hardcoreLadder, setHardcoreLadder] = useState([])
-  const[ grab, setGrab] = useState(false)
-  const [msg, setMsg] = useState(null)
-  const [ history, setHistory ] = useState([])
+    // if someone if currently logged in or not
+  const [loggedIn, setLoggedIn] = useState(false);
+    // an array of character objects that have been favourited by the active user (cookies.user)
+  const [favourites, setFavourites] = useState([]);
+    // an array of favourites this logged in user has
+  const [loadingMsg, setLoadingMsg] = useState(null);
+    // if there is an error loading, this message displays in Loading.js component
+  const [loadingError, setLoadingError] = useState(null);
+    // if ladder is set to standard
+  const [standardLadder, setStandardLadder] = useState([]);
+    // if ladder is set to hardcore
+  const [hardcoreLadder, setHardcoreLadder] = useState([]);
+    // used to seed the database for development 
+  const [grab, setGrab] = useState(false);
+    // content of different messages used
+  const [msg, setMsg] = useState(null);
+    // an array of states, used to navigate using the back button
+  const [history, setHistory] = useState([]);
 
   useEffect(() => {
-    setState('loading')
-    setLoadingMsg('Grabbing characters...')
+    setState("loading");
+    setLoadingMsg("Grabbing characters...");
 
     setTimeout(() => {
+      axios
+        .get("http://localhost:3030/characters")
+        .then((res) => {
+          // compares characters saved in the DB to all characters in the ladder.json and filters out and characters
+          // that don't exist in the DB
+          const ladder_hardcore_display = ladder_hardcore.entries.reduce(
+            (accumulator, currentValue) => {
+              if (
+                res.data.filter(
+                  (element) => element.name === currentValue.character.name
+                ).length > 0
+              ) {
+                return [...accumulator, currentValue];
+              } else {
+                return [...accumulator];
+              }
+            },
+            []
+          );
+          const ladder_standard_display = ladder_standard.entries.reduce(
+            (accumulator, currentValue) => {
+              if (
+                res.data.filter(
+                  (element) => element.name === currentValue.character.name
+                ).length > 0
+              ) {
+                return [...accumulator, currentValue];
+              } else {
+                return [...accumulator];
+              }
+            },
+            []
+          );
 
-    axios.get('http://localhost:3030/characters')
-    .then((res) => {
-      console.log('characters from server: ',   res)
-      const ladder_hardcore_display = ladder_hardcore.entries.reduce((accumulator, currentValue) => {
-        if (res.data.filter(element => element.name === currentValue.character.name).length > 0) {
-          return [...accumulator, currentValue]
-        } else {
-          return [...accumulator]
-        }
-        
-    }, [])
-    const ladder_standard_display = ladder_standard.entries.reduce((accumulator, currentValue) => {
-      if (res.data.filter(element => element.name === currentValue.character.name).length > 0) {
-        return [...accumulator, currentValue]
-      } else {
-        return [...accumulator]
-      }
-    
-      
-    }, [])
+          setStandardLadder(ladder_standard_display);
+          setHardcoreLadder(ladder_hardcore_display);
+        })
+        .then(() => {
+          setState("ladder");
+        });
+    }, 1000);
+  }, []);
 
-    setStandardLadder(ladder_standard_display)
-    setHardcoreLadder(ladder_hardcore_display)
-
-  }).then(() => {
-    setState('ladder')
-  })
-}, 1000)
-}, []);
-
-useEffect(() => {
-  if (cookies.user) {
-    axios.get(`http://localhost:3030/users/${cookies.user}/favourites`).then((result) => {
-      console.log('favs from server: ', result)
-      if (result) {
-      setFavourites(result.data)
-      } 
-    }).catch((err) => {
-      console.log(err)
-    })
-  } else {
-    setFavourites([])
-  }
-}, [])
-
-useEffect(() => {
-  if (state !== 'loading' && history[history.length - 1] !== state) {
-      setHistory([...history, state])
-  }
-
-}, [state])
-
-useEffect(() => {
-  if (cookies.user) {
-    setLoggedIn(true)
-  }
-})
-
-function back() {
-  if (history.length >= 1) {
-    const previous = history[history.length -1];
-    const newHistory = history
-    newHistory.splice(-1)
-    setHistory(newHistory)
-    setState(previous)
-  }
-}
-                                  
-  function removeFavourite(name) {
-   axios.delete(`http://localhost:3030/users/${cookies.user}/favourites/${name}`).then((result) => {
-      setFavourites(result.data)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }
-
-  function addFavourite(name) {
-    axios.post(`http://localhost:3030/users/${cookies.user}/favourites/${name}`).then((result) => {
-      setFavourites(result.data)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }
-
-  function getCharacter(accountName, characterName) {
-
-    const randomInterval = function () {
-      return Math.floor((Math.random() * 500) + 100)
+  // get user characters if user is logged in (cookies.user)
+  useEffect(() => {
+    if (cookies.user) {
+      axios
+        .get(`http://localhost:3030/users/${cookies.user}/favourites`)
+        .then((result) => {
+          if (result) {
+            setFavourites(result.data);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setFavourites([]);
     }
+  }, []);
+
+  // app keeps a record of state changes so we can go 'back'
+  useEffect(() => {
+    if (state !== "loading" && history[history.length - 1] !== state) {
+      setHistory([...history, state]);
+    }
+  }, [state]);
+
+  useEffect(() => {
+    if (cookies.user) {
+      setLoggedIn(true);
+    }
+  });
+
+  function back() {
+    if (history.length >= 1) {
+      const previous = history[history.length - 1];
+      const newHistory = history;
+      newHistory.splice(-1);
+      setHistory(newHistory);
+      setState(previous);
+    }
+  }
+
+  // parameter is a string of the character's name i.e. 'SoundsOfViolence'
+  // output is the entire array of favourited characters (as objects) 
+  function removeFavourite(name) {
+    axios
+      .delete(`http://localhost:3030/users/${cookies.user}/favourites/${name}`)
+      .then((result) => {
+        setFavourites(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // parameter is a string of the character's name i.e. 'SoundsOfViolence'
+  // output is the entire array of favourited characters (as objects) 
+  function addFavourite(name) {
+    axios
+      .post(`http://localhost:3030/users/${cookies.user}/favourites/${name}`)
+      .then((result) => {
+        setFavourites(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // to get a specific character's info to display in Character.js
+  // parameter: accountName (string, i.e. 'Panini') and characterName (string, i.e. 'SoundsOfViolence')
+  // result: sets the current character to an object containing all character and account information
+  function getCharacter(accountName, characterName) {
+    const randomInterval = function () {
+      return Math.floor(Math.random() * 500 + 100);
+    };
     setState("loading");
     setTimeout(() => {
       fetchCharacter(accountName, characterName).then((res) => {
@@ -156,46 +190,104 @@ function back() {
         }
         setState("character");
       });
-    }, randomInterval())
-  };
+    }, randomInterval());
+  }
 
   return (
     <Container fluid>
-      {history.length > 1 && <div id="back-button" onClick={back}><CgChevronDoubleLeft onClick={back} size="6em" /></div>}
-      <Navigation state={state} setGrab={setGrab} grab={grab} getCharacter={getCharacter} setState={setState} removeCookie={removeCookie} cookies={cookies} setAccount={setAccount} />
-      {msg && <Message msg={msg} setMsg={setMsg}/>}
-      <Grabber grab={grab}/>
+      {history.length > 1 && (
+        <div id="back-button" onClick={back}>
+          <CgChevronDoubleLeft onClick={back} size="6em" />
+        </div>
+      )}
+      <Navigation
+        state={state}
+        setGrab={setGrab}
+        grab={grab}
+        getCharacter={getCharacter}
+        setState={setState}
+        removeCookie={removeCookie}
+        cookies={cookies}
+        setAccount={setAccount}
+      />
+      {msg && <Message msg={msg} setMsg={setMsg} />}
+      <Grabber grab={grab} />
       <ScrollUpButton />
       <Container style={{ marginTop: "100px" }}>
         <Switch>
-      <Route exact path="/">
+          <Route exact path="/">
+            {state === "account" && (
+              <Account
+                account={account}
+                getCharacter={getCharacter}
+                setState={setState}
+                setLoadingMsg={setLoadingMsg}
+                setLoadingError={setLoadingError}
+                cookies={cookies}
+              />
+            )}
+            {state === "ladder" && (
+              <Ladder
+                getCharacter={getCharacter}
+                setState={setState}
+                standard={standardLadder}
+                hardcore={hardcoreLadder}
+                favourites={favourites}
+                addFavourite={addFavourite}
+                removeFavourite={removeFavourite}
+                setMsg={setMsg}
+                cookies={cookies}
+              />
+            )}
 
-         {state === "account" && <Account account={account} getCharacter={getCharacter} setState={setState} setLoadingMsg={setLoadingMsg} setLoadingError={setLoadingError} cookies={cookies}/>}
-            {state === "ladder" && <Ladder getCharacter={getCharacter} setState={setState} standard={standardLadder} hardcore={hardcoreLadder} favourites={favourites} addFavourite={addFavourite} removeFavourite={removeFavourite} setMsg={setMsg} cookies={cookies}/>}
+            {state === "character" && character && (
+              <Character
+                character={character}
+                view={state}
+                addFavourite={addFavourite}
+                removeFavourite={removeFavourite}
+                favourites={favourites}
+                cookies={cookies}
+                setState={setState}
+                setAccount={setAccount}
+                msg={msg}
+                setMsg={setMsg}
+              />
+            )}
 
-          {state === 'character' && character && <Character
-            character={character}
-            view={state}
-            addFavourite={addFavourite}
-            removeFavourite={removeFavourite}
-            favourites={favourites}
-            cookies={cookies}
-            setState={setState}
-            setAccount={setAccount}
-            msg={msg}
-            setMsg={setMsg}
-          />}
+            {state === "favourites" && (
+              <Favourites
+                favourites={favourites}
+                removeFavourite={removeFavourite}
+                getCharacter={getCharacter}
+                setState={setState}
+              />
+            )}
 
-        {state === 'favourites' && <Favourites favourites={favourites} removeFavourite={removeFavourite} getCharacter={getCharacter} setState={setState}/>}
-
-        {state === "character" && !character && (
-          <Loading error={loadingError} msg={loadingMsg} setState={setState} />
-        )}
-        {state === "loading" && <Loading error={loadingError} msg={loadingMsg} setState={setState}/>}
-        </Route>
-        <Route path="/register">
-          {!loggedIn && <Register handleCookie={handleCookie} setState={setState} setLoggedIn={setLoggedIn}/>}
-          {loggedIn && <Redirect to="/"/>}
+            {state === "character" && !character && (
+              <Loading
+                error={loadingError}
+                msg={loadingMsg}
+                setState={setState}
+              />
+            )}
+            {state === "loading" && (
+              <Loading
+                error={loadingError}
+                msg={loadingMsg}
+                setState={setState}
+              />
+            )}
+          </Route>
+          <Route path="/register">
+            {!loggedIn && (
+              <Register
+                handleCookie={handleCookie}
+                setState={setState}
+                setLoggedIn={setLoggedIn}
+              />
+            )}
+            {loggedIn && <Redirect to="/" />}
           </Route>
           <Route path="/logout">
             <Logout />
@@ -204,15 +296,8 @@ function back() {
             {!loggedIn && <Login handleCookie={handleCookie} />}
             {loggedIn && <Redirect to="/" />}
           </Route>
-
-      
-
         </Switch>
       </Container>
-     
-
-
     </Container>
   );
 }
-
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import Items from './Items';
-import Flasks from './Flasks'
-import LikeButton from './LikeButton'
+import React from "react";
+import Items from "./Items";
+import Flasks from "./Flasks";
+import LikeButton from "./LikeButton";
 
-import Skills from './Skills'
+import Skills from "./Skills";
 
-import './Character.scss'
-import './Skills.scss'
+import "./Character.scss";
+import "./Skills.scss";
 
+import { Row, Col } from "react-bootstrap";
 
-import { Row, Col } from 'react-bootstrap'
+import { useState, useEffect } from "react";
 
-import { useState, useEffect } from 'react'
-
-const classNames = require('classnames')
+const classNames = require("classnames");
 
 export default function Character(props) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-  let arrangedItems = []
+  let arrangedItems = [];
   let helm;
   let gloves;
   let boots;
@@ -30,86 +28,132 @@ export default function Character(props) {
   let ring2;
   let amulet;
 
-
   for (const item of props.character.items.items) {
     if (item.inventoryId === "Helm") {
-      helm = item
+      helm = item;
     } else if (item.inventoryId === "Gloves") {
-      gloves = item
+      gloves = item;
     } else if (item.inventoryId === "Boots") {
-      boots = item
+      boots = item;
     } else if (item.inventoryId === "Offhand") {
-      offhand = item
+      offhand = item;
     } else if (item.inventoryId === "Weapon") {
-      weapon = item
+      weapon = item;
     } else if (item.inventoryId === "BodyArmour") {
-      bodyarmour = item
+      bodyarmour = item;
     } else if (item.inventoryId === "Ring") {
-      ring = item
+      ring = item;
     } else if (item.inventoryId === "Ring2") {
-      ring2 = item
+      ring2 = item;
     } else if (item.inventoryId === "Amulet") {
-      amulet = item
+      amulet = item;
     }
   }
 
-  arrangedItems = [helm, gloves, boots, offhand, weapon, bodyarmour, ring, ring2, amulet]
+  arrangedItems = [
+    helm,
+    gloves,
+    boots,
+    offhand,
+    weapon,
+    bodyarmour,
+    ring,
+    ring2,
+    amulet,
+  ];
 
-  const filteredArrangedItems = arrangedItems.reduce(function (result, element) {
+  const filteredArrangedItems = arrangedItems.reduce(function (
+    result,
+    element
+  ) {
     if (element) {
-      result.push(element)
+      result.push(element);
     }
-    return result
-  }, []);
+    return result;
+  },
+  []);
 
-
-  const gems = filteredArrangedItems.map(item => {
-    if (item.inventoryId === "Offhand2" || item.inventoryId === "Weapon2" || item.inventoryId === "Belt") {
+  const gems = filteredArrangedItems.map((item) => {
+    if (
+      item.inventoryId === "Offhand2" ||
+      item.inventoryId === "Weapon2" ||
+      item.inventoryId === "Belt"
+    ) {
     } else {
       if (item.socketedItems) {
-        return <Skills item={item} itemName={item.inventoryId} gems={item.socketedItems} />
+        return (
+          <Skills
+            item={item}
+            itemName={item.inventoryId}
+            gems={item.socketedItems}
+          />
+        );
       }
     }
-  })
+  });
 
   useEffect(() => {
     function handleResize() {
-      setWindowWidth(window.innerWidth)
+      setWindowWidth(window.innerWidth);
     }
-    window.addEventListener("resize", handleResize)
-  })
+    window.addEventListener("resize", handleResize);
+  });
 
-  const charClass = props.character.class
-  const classIcon = `/icons/${charClass.toLowerCase()}_icon.png`
+  const charClass = props.character.class;
+  const classIcon = `/icons/${charClass.toLowerCase()}_icon.png`;
   return (
-
     <div className="container character-container char">
       <Row className="p-5 char-title">
-        <Col lg="auto" ><img src={classIcon} alt={props.character.class} /></Col>
+        <Col lg="auto">
+          <img src={classIcon} alt={props.character.class} />
+        </Col>
         <Col lg={9} className="my-auto">
           <h1 className="character-name">{props.character.name}</h1>
-          <h5 >{props.character.level} | {props.character.class}</h5>
-          <h5><a href="/" onClick={(e) => {
-            e.preventDefault();
-            props.setAccount(props.character.account_name)
-            props.setState('account')
-          }}className="accountLink">{props.character.account_name}</a></h5>
+          <h5>
+            {props.character.level} | {props.character.class}
+          </h5>
+          <h5>
+            <a
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+                props.setAccount(props.character.account_name);
+                props.setState("account");
+              }}
+              className="accountLink"
+            >
+              {props.character.account_name}
+            </a>
+          </h5>
         </Col>
         <Col>
-          {props.cookies.user && <LikeButton character={props.character} favourites={props.favourites} addFavourite={props.addFavourite} removeFavourite={props.removeFavourite} size="4em" msg={props.msg} setMsg={props.setMsg} />}
+          {props.cookies.user && (
+            <LikeButton
+              character={props.character}
+              favourites={props.favourites}
+              addFavourite={props.addFavourite}
+              removeFavourite={props.removeFavourite}
+              size="4em"
+              msg={props.msg}
+              setMsg={props.setMsg}
+            />
+          )}
         </Col>
-
-
       </Row>
-      <Row className="p-3" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
-
+      <Row className="p-3" style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
         <Col lg={12}>
           <Row className="poe-container">
-            <Items windowWidth={windowWidth} items={props.character.items.items} />
+            <Items
+              windowWidth={windowWidth}
+              items={props.character.items.items}
+            />
           </Row>
           <Row>
             <Col>
-              <Flasks windowWidth={windowWidth} items={props.character.items.items} />
+              <Flasks
+                windowWidth={windowWidth}
+                items={props.character.items.items}
+              />
             </Col>
           </Row>
         </Col>
@@ -117,7 +161,6 @@ export default function Character(props) {
           {gems}
         </Row>
       </Row>
-
     </div>
   );
-};
+}
