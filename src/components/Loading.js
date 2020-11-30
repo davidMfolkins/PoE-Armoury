824244468d45761b703303f5e70183c8879ec46d
erikleisinger <erikleisinger@gmail.com>
2020-11-30 14:24:52 -0500

    items corners fixed

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,30 +1,36 @@
-import './Loading.scss'
+import Spinner from "react-bootstrap/Spinner";
+import "./Loading.scss";
 
-import Spinner from 'react-bootstrap/Spinner'
 
+// props.error determines if loading should stop and display error msg
+// props.msg is the actual message displayed while the loading spinner spins
 export default function Loading(props) {
-
-
-  return <div className="container" id="container">
-    <div style={{textAlign: 'center', height: '50px'}}>
-    {!props.error && 
-    <div>
-      <Spinner style={{width: '5em', height: '5em'}} animation="border" variant="warning" >
-        <span className="sr-only">Loading...</span>
-      </Spinner>
+  return (
+    <div className="container" id="container">
+      <div style={{ textAlign: "center", height: "50px" }}>
+        {!props.error && (
+          <div>
+            <Spinner
+              style={{ width: "5em", height: "5em" }}
+              animation="border"
+              variant="warning"
+            >
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          </div>
+        )}
+        {props.msg && <div style={{ color: "white" }}>{props.msg}</div>}
+        {props.error && (
+          <div style={{ color: "white" }}>
+            {props.error}
+            <div>
+              <a href="#" onClick={() => props.setState("ladder")}>
+                Back
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-    
-  }
-  {props.msg && <div style={{color: 'white'}}>{props.msg}</div>}
-  {props.error && 
-  <div style={{color: 'white'}}>
-    {props.error}
-    <div>
-      <a href="#" onClick={() => props.setState('ladder')}>Back</a>
-    </div>
-  </div>}
-
-  </div>
-</div>
-
+  );
 }
