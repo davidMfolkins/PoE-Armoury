ca65360e14fd5b6efc101c4e468ab26531919280
erikleisinger <erikleisinger@gmail.com>
2020-11-30 13:39:20 -0500

    favourites.js and favourites.scss refactor

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -1,79 +1,113 @@
-import { Button } from 'react-bootstrap'
-import { AiFillDelete } from 'react-icons/ai'
+import { useState } from "react";
 
-import { useState } from 'react'
-
-import './Favourite.scss'
+import "./Favourite.scss";
+import { AiFillDelete } from "react-icons/ai";
 
 export default function Favourite(props) {
-
-  const [msg, setMsg] = useState(null)
-
-  const [deleted, setDeleted] = useState(false)
+  // setting the deleted/not deleted msgs
+  const [msg, setMsg] = useState(null);
+  // for delete transitions
+  const [deleted, setDeleted] = useState(false);
 
   async function handleRemoveFavourite(name) {
-
-    setDeleted(true)
+    setDeleted(true);
     setTimeout(async () => {
-      setDeleted(false)
-      setMsg(null)
-      await props.removeFavourite(name)
-    }, 2000)
-
+      setDeleted(false);
+      setMsg(null);
+      await props.removeFavourite(name);
+    }, 2000);
   }
 
   function removeCheck() {
-    setMsg('Are you sure you want to delete?')
+    setMsg("Are you sure you want to delete?");
   }
-
+  // for clicking a character and going to their character page
   const handleCharacterChange = function (account, character, id) {
     props.getCharacter(account, character);
-  }
+  };
+
   const classIcon = `/icons/${props.fav.class.toLowerCase()}_icon.png`;
+
   if (!msg) {
-
-  return (
-    <tr id="favouriteList" className="d-flex">
-      <td id="classCell" className="col-3" onClick={(e) => {
-        e.preventDefault()
-        handleCharacterChange(props.fav.account_name, props.fav.name, props.fav.id)
-      }
-      }><div><img src={classIcon} alt={props.fav.class} /> {props.fav.class}</div></td>
-      <td className="col-3" onClick={(e) => {
-        e.preventDefault()
-        handleCharacterChange(props.fav.account_name, props.fav.name, props.fav.id)
-      }
-      }>{props.fav.name}</td>
-      <td className="col-3" onClick={(e) => {
-        e.preventDefault()
-        handleCharacterChange(props.fav.account_name, props.fav.name, props.fav.id)
-      }
-      }>{props.fav.level}</td>
-      <td className="col-3">
-        
-          <AiFillDelete size="2em" onClick={() => removeCheck()}  />
-
-      </td>
-    </tr>
-    )
+    return (
+      <tr id="favouriteList" className="d-flex">
+        <td
+          id="classCell"
+          className="col-3"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCharacterChange(
+              props.fav.account_name,
+              props.fav.name,
+              props.fav.id
+            );
+          }}
+        >
+          <div>
+            <img src={classIcon} alt={props.fav.class} /> {props.fav.class}
+          </div>
+        </td>
+        <td
+          className="col-3"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCharacterChange(
+              props.fav.account_name,
+              props.fav.name,
+              props.fav.id
+            );
+          }}
+        >
+          {props.fav.name}
+        </td>
+        <td
+          className="col-3"
+          onClick={(e) => {
+            e.preventDefault();
+            handleCharacterChange(
+              props.fav.account_name,
+              props.fav.name,
+              props.fav.id
+            );
+          }}
+        >
+          {props.fav.level}
+        </td>
+        <td className="col-3">
+          <AiFillDelete size="2em" onClick={() => removeCheck()} />
+        </td>
+      </tr>
+    );
   } else if (deleted) {
     return (
       <tr id="favouriteList" className="d-flex">
-
-        <td className="col-12" style={{ backgroundColor: 'rgba(249, 189, 189, 0.5', color: 'white' }} id="deleted">Deleted!
-      </td>
+        <td
+          className="col-12"
+          style={{ backgroundColor: "rgba(249, 189, 189, 0.5", color: "white" }}
+          id="deleted"
+        >
+          Deleted!
+        </td>
       </tr>
-    )
+    );
   } else {
     return (
       <tr id="favouriteList" className="d-flex">
-
-        <td className="col-12 trash" style={{ backgroundColor: 'rgba(249, 189, 189, 0.5', color: 'white' }}>
-          <a href="#" onClick={() => handleRemoveFavourite(props.fav.character_name)} variant="primary" size="lg">Are you sure you want to remove from favourites?
-          <AiFillDelete size="3em" ></AiFillDelete>
-          </a>{' '}
+        <td
+          className="col-12 trash"
+          style={{ backgroundColor: "rgba(249, 189, 189, 0.5", color: "white" }}
+        >
+          <a
+            href="#"
+            onClick={() => handleRemoveFavourite(props.fav.character_name)}
+            variant="primary"
+            size="lg"
+          >
+            Are you sure you want to remove from favourites?
+            <AiFillDelete size="3em"></AiFillDelete>
+          </a>{" "}
         </td>
       </tr>
-    )
+    );
   }
 }
